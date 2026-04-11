3bfd4b352bc1d646e2858562bf5850f3c27a6b20
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-04-11 18:01:20 +0530

    Add files via upload

---
+++ longest_validparen.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int longestValidParentheses(string s) {
+    stack<int> st;
+    st.push(-1);  
+    int maxLen = 0;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '(') {
+            st.push(i);
+        } else {
+            st.pop();
+            if(st.empty()) {
+                st.push(i); 
+            } else {
+                maxLen = max(maxLen, i - st.top());
+            }
+        }
+    }
+    return maxLen;
+}
+int main(){
+    string s;
+    cin >> s;
+    int result = longestValidParentheses(s);
+    cout << "Length of the longest valid parentheses substring: " << result << endl;
+    return 0;
+}
